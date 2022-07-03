0e92d6fac73c258232a6297194aa6aef3a88c3fb
Irakly Gagua <iraklygagua@gmail.com>
2022-07-03 05:50:56 +0300

    continue developing

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 import GameWorld.Game;
 
-import java.io.IOException;
-import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class Main {
     public static void main(String[] args) {
         Game.start();
